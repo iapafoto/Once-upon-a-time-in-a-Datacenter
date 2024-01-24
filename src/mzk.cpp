8b3e4edb682b7730cca6e702a9902150aaa13d0a
Sébastien Durand <33156785+iapafoto@users.noreply.github.com>
2024-01-24 14:50:05 +0100

    quelques octets en moins

--- src/mzk.cpp
+++ src/mzk.cpp
@@ -19,12 +19,12 @@ float hash(float x) {
     return fract(sin(x * 43758.54f) );
 }
 
-float clamp(float a, float n, float x) {
-    return a < n ? n : a > x ? x : a;
+float clamp(float a, float n/*, float x*/) {
+    return a < n ? n : a > 1.f ? 1.f : a;
 }
 
-float smoothstep(float x) {  // smoothstep(0,1,x)
-    x = clamp(x, p0d00, p1d00);
+float smoothstep(float x) {  // smoothstep(0,1,x)  4079  + 5 en inline
+    x = clamp(x, p0d00);
     return x * x * (3.f - x-x);
 }
 
@@ -32,7 +32,7 @@ float beat(float time) {
     float t = fract(time);
     float f = 220.f * exp(-5.f * t);
     float a = sin(3.141592f * f * t);
-    float v = clamp(a * 8.f, -1.f, 1.f) * (exp(-10.f * t) + exp(-t));
+    float v = clamp(a * 8.f, -1.f) * (exp(-10.f * t) + exp(-t));
     v += a * 8.f * exp(-t); 
     return v + .8f * a * exp(- 3.f * t - p0d50);  // 4076
 }
@@ -45,8 +45,8 @@ float melody(float time) {
 //    v += .3f * sin(3.141592f * f * time  + 8.f * im * sin(3.141592f * f * time)); // cette ligne rajoute 50 octets !!!
     return v * exp(-2.f * fract(8.f * time));
 }
-//4076 (apres pusieurs rebuild !??
-void mzk_init(short* buffer) {  //4115    15 octet le smoothstep
+//4071
+void mzk_init(short* buffer) {
     for (int i = 0; i < MZK_NUMSAMPLES; i++) {
         float t = (float)i / (float)MZK_RATE;
         float tb = t+exp(.0025f * t);
@@ -55,6 +55,6 @@ void mzk_init(short* buffer) {  //4115    15 octet le smoothstep
         // voir avec sin(x)/x -> plus leger que smoothstep peu etre
         y += .4f * (1.f - smoothstep(fabs(t - 67.4f)/p0d20) * smoothstep(fabs(t - 80.f)/p0d40)) * melody(tb * 80.f);
         y += (p1d00 - sm) * (p0d05 * (sin(5.f * t + hash(t)) + beat(tb)) + p0d08 * beat(tb - p0d70));
-        buffer[i] = f2i(y* 4915.f/*p0d15 * 32767.f*/);
+        buffer[i] = f2i(y* 4915.f);
     }
 }
