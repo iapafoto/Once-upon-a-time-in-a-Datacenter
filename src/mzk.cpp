dcb778eb34064a11d4f0a882711dddd139f66704
Sébastien Durand <33156785+iapafoto@users.noreply.github.com>
2024-01-25 20:11:42 +0100

    micro math optims - 4085 octets

--- src/mzk.cpp
+++ src/mzk.cpp
@@ -21,9 +21,13 @@ float fract(float a) {
 float hash(float x) {
     return fract(sinf(x * 43758.54f) );
 }
-
+/*
 float clamp(float a, float n) {  //clamp(a,n,1)
     return a < n ? n : a > 1.f ? 1.f : a;
+}*/
+float clamp(float x, float a) // -3 octets 
+{
+    return .5f * (fabs(x - a) + a + 1.f - fabs(x - 1.f));
 }
 
 //float smoothstep(float x) {  // ramp(0,1,x)  4079  + 5 en inline
@@ -38,7 +42,6 @@ float ksinf(float x, float k) {  // ramp(0,1,x)  4079  + 5 en inline
 
 float beat(float time) {
     float t = fract(time);
-  //  float f = 220.f * mexpf(-5.f * t);
     float a = sinf(691.15f * mexpf(-5.f * t) * t);
     float v = clamp(a * 8.f, -1.f) * (mexpf(-10.f * t) + mexpf(-t));
     v += a * 8.f * mexpf(-t); 
@@ -48,28 +51,20 @@ float beat(float time) {
 float melody(float time) {
    // const float f = 440.f;
     float im = 10.f + 9.f * sinf(1.57079f * time);
-
-   // float v = sinf(6.2831f * f * time + im * ksinf(1.57079f * f * time, ramp((time-95.f)/30.f)));
+       // float v = sinf(6.2831f * f * time + im * ksinf(1.57079f * f * time, ramp((time-95.f)/30.f)));
     float v = sinf(2764.564f * time + im * ksinf(691.1476f * time, ramp((time - 95.f) / 30.f)));
-  
-
-    //v += .3f * sinf(3.141592f * f * time); // cette ligne rajoute 17 octets
-//    v += .3f * sinf(3.141592f * f * time  + 8.f * im * sinf(3.141592f * f * time)); // cette ligne rajoute 50 octets !!!
     return v * mexpf(-2.f * fract(8.f * time));
 }
 
 
-//4070
 void mzk_init(short* buffer) {
     for (int i = 0; i < MZK_NUMSAMPLES; i++) {
         float t = (float)i / (float)MZK_RATE;
         float tb = t + mexpf(.0025f * t);
         float sm = ramp((t - 25.f) / 60.f);
         float y = p0d30 * melody(tb)*sm;
-        y += .4f * (1.f - ramp(fabs(t - 67.4f)/p0d20) * ramp(fabs(t - 80.f)/p0d40)) * melody(tb * 80.f);
+        y += .4f * (1.f - ramp(fabs(t - 67.4f)/p0d20) * ramp(fabs(t - 79.9f)/p0d40)) * melody(tb * 80.f);
         y += (p1d00 - sm) * (p0d05 * (sinf(5.f * t + hash(t)) + beat(tb)) + p0d08 * beat(tb - p0d70));
-        //y += wind(t);
-        buffer[i] = f2i( y * 4915.f);
+        buffer[i] = f2i( y * 9830.f);
     }
 }
-//4087
